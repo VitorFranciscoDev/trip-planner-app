dc1a791d814aecf2f215a01461135842c5560005
Vitor Francisco <vitorfndr@gmail.com>
2025-10-26 21:36:48 -0300

    feat(): person module done

--- lib/modules/person/person_usecase.dart
+++ lib/modules/person/person_usecase.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:trip_planner/infrastructure/presentation/app/app_localizations.dart';
 
+// Person's Use Cases
 class PersonUseCase {
+  // Validation of Name
   String? validateName(String name, BuildContext context) {
     final l10n = AppLocalizations.of(context);
     return name.isEmpty ? l10n.nameRequired : null;
   }
 
+  // Validation of Age
   String? validateAge(String age, BuildContext context) {
     final l10n = AppLocalizations.of(context);
     
